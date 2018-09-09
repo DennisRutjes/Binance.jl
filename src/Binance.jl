fe93b73632d5868457c0c222102b211fb6094a82
Dennis Rutjes <dennis@jpoint.nl>
2018-09-09 16:09:01 +0200

    added rest calls

--- src/Binance.jl
+++ src/Binance.jl
@@ -6,35 +6,55 @@ apiKey = ENV["BINANCE_APIKEY"]
 apiSecret = ENV["BINANCE_SECRET"] 
 
 # base URL of the Binance API
-API_REST = "https://api.binance.com/"
-API_WS = "wss://stream.binance.com:9443/ws/"
-API_STREAM = "wss://stream.binance.com:9443/stream/"
+BINANCE_API_REST = "https://api.binance.com/"
+BINANCE_API_TICKER = string(BINANCE_API_REST,"api/v1/ticker/")
+INANCE_API_KLINES = string(BINANCE_API_REST,"api/v1/klines/")
+
+BINANCE_API_WS = "wss://stream.binance.com:9443/ws/"
+BINANCE_API_STREAM = "wss://stream.binance.com:9443/stream/"
+
 # function HTTP response 2 JSON
 function r2j(response)
     JSON.parse(String(response))
 end
 
 # retrieve data from binance for "allPrices", "24hr", "allBookTickers"
-function getBinanceDataFor(withSubject)
-    r = HTTP.request("GET", string(BINANCE_API_REST, withSubject))
-    r2j(r.body)
-    
+function getAllPrices()
+    r = HTTP.request("GET", string(BINANCE_API_TICKER, "allPrices"))
+    r2j(r.body)    
 end
 
-# current Binance market
-function getBinanceMarket()
+function get24HR()
+    r = HTTP.request("GET", string(BINANCE_API_TICKER, "24hr"))
+    r2j(r.body)    
+end
+
+function get24HR(symbol::String)
+    r = HTTP.request("GET", string(BINANCE_API_TICKER, "24hr?symbol=", symbol))
+    r2j(r.body)    
+end
+
+function getAllBookTickers()
     r = HTTP.request("GET", "https://www.binance.com/exchange/public/product")
     r2j(r.body)["data"]
 end
 
-# binance get candlesticks/klines data
-function getBinanceKline(symbol, startDateTime, endDateTime ; interval="1m")
-    startTime = @sprintf("%.0d",Dates.datetime2unix(startDateTime) * 1000)
-    endTime = @sprintf("%.0d",Dates.datetime2unix(endDateTime) * 1000)
-    query = string("symbol=", symbol, "&interval=", interval, "&startTime=", startTime, "&endTime=", endTime)
-    r = HTTP.request("GET", string("https://api.binance.com/api/v1/klines?",  query))
-    r2j(r.body)
+function getAllBookTickers(symbol::String)
+    r = HTTP.request("GET", "https://www.binance.com/exchange/public/product?symbol=",symbol)
+    r2j(r.body)["data"]
 end
 
+# binance get candlesticks/klines data
+function getKlines(symbol; startDateTime, endDateTime, interval="1m")
+    query = "symbol=$symbol&interval=$interval"
+    
+    if startDateTime && endDateTime
+        startTime = @sprintf("%.0d",Dates.datetime2unix(startDateTime) * 1000)
+        endTime = @sprintf("%.0d",Dates.datetime2unix(endDateTime) * 1000)
+        query = string(query, "&startTime=", startTime, "&endTime=", endTime)
+    end
+   r = HTTP.request("GET", string(INANCE_API_KLINES,"?",  query))
+    r2j(r.body)
+end
 
 end
