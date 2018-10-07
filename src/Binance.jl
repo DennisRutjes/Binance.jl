8e71df5187d45a0e106f51cccbf009a010b8fe48
Dennis Rutjes <dennis@jpoint.nl>
2018-10-07 22:41:27 +0200

    async

--- src/Binance.jl
+++ src/Binance.jl
@@ -286,7 +286,7 @@ function wsKlineStreams(callback::Function, symbols::Array, interval="1m")
     #interval => 1m 3m 5m 15m 30m 1h 2h 4h 6h 8h 12h 1d 3d 1w 1M
       allStreams = map(s -> string(lowercase(s), "@kline_", interval), symbols)
       error = false;
-      HTTP.WebSockets.open(string(BINANCE_API_WS,join(allStreams, "/")); verbose=false) do io
+      @async HTTP.WebSockets.open(string(BINANCE_API_WS,join(allStreams, "/")); verbose=false) do io
         while !eof(io);
             data = String(readavailable(io))
             callback(data)
